3938dbbf047dcbccf44ee1ecff2ac760c1ba569d
Kseniia66 <kseniyau996@yandex.ru>
2025-03-26 19:38:58 +0400

    step1

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ build:
 
 start:
 	npx start-server -s ./frontend/dist
+
+develop:
+	npx start-server & npm -C frontend start
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    // Проксируем запросы к API
-    '/api': {
-      target: 'http://localhost:5001',
-    },
-    // Проксируем WebSocket соединения
-    '/socket.io': {
-      target: 'ws://localhost:5001',
-      ws: true,
-      rewriteWsOrigin: true,
+  server: {
+    port: 5002,
+    proxy: {
+      // Проксируем запросы к API
+      '/api': {
+        target: 'http://localhost:5001',
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
     },
   },
-})
+});
